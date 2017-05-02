87bd34528f907b4c76691cbd68b4fdbd11f2b400
Dawid Ryłko <dawid.rylko@gmail.com>
2017-05-02 17:53:56 +0200

    Fix sidebar route

--- src/app/product/product.routing.ts
+++ src/app/product/product.routing.ts
@@ -1,7 +1,7 @@
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
 
-import { ProductListComponent } from './product-list/product-list.component'
-import { ProductFormComponent } from './product-form/product-form.component'
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
 const routes: Routes = [
   { path: 'product',
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { SidebarComponent } from './sidebar/sidebar.component'
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     SidebarComponent
